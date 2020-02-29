36b5ba0466b070d515664f4e71a3618108530219
briancodes <15702512+briancodes@users.noreply.github.com>
2020-02-29 21:48:39 +0000

    Refactoring of cors and app.listen - cors only applied to specific endpoint - app.listen using port and hostname

--- module-1/local/local-server.js
+++ module-1/local/local-server.js
@@ -4,6 +4,8 @@ const csp = require('helmet-csp');
 const cors = require('cors');
 
 const app = express();
+const hostname = 'localhost';
+const port = 4200;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,4 +17,6 @@ app.get('/local-api', (req, res) => {
   res.status(200).json({ result: 'Local Success' });
 });
 
-app.listen(4200);
+app.listen(port, hostname, () => {
+  console.log(`Local server started http://${hostname}:${port}`);
+});
--- module-1/remote/remote-server.js
+++ module-1/remote/remote-server.js
@@ -4,17 +4,21 @@ const csp = require('helmet-csp');
 const cors = require('cors');
 
 const app = express();
+const hostname = 'localhost';
+const port = 8000;
 
+// CORS for fetch & XMLHttpRequest
+// https://flaviocopes.com/express-cors/
 const corsOptions = {
   origin: 'http://localhost:4200',
 };
 
-app.use(cors(corsOptions));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/remote-api', (req, res) => {
+app.get('/remote-api', cors(corsOptions), (req, res, next) => {
   res.status(200).json({ result: 'Remote Success' });
 });
 
-app.listen(8000);
+const server = app.listen(port, hostname, () => {
+  console.log(`Remote server started http://${hostname}:${port}`);
+});
